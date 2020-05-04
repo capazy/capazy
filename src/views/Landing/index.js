f43ffa573cbc4b55630fbc99811baa025f2de2c5
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-05-03 22:51:24 -0500

    Create new signup page. Fix all warnigs and error.

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Support = () => {
   const {
@@ -13,88 +13,87 @@ const Support = () => {
     touched,
   } = useFormik({
     initialValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
     validationSchema: Yup.object({
-      email: Yup.string().email("Invalid email address").required("Required"),
+      email: Yup.string().email('Invalid email address').required('Required'),
       password: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
     }),
     onSubmit: (values) => {},
   });
 
   return (
-    <div className='bg-gray-100 '>
-      <div className='md:flex ml-auto text-center justify-center py-20 px-5'>
-        <form className='w-full max-w-lg' onSubmit={handleSubmit}>
-          <h2 className='text-2xl text-gray-600 font-bold text-center mb-10'>
+    <div className="bg-gray-100 ">
+      <div className="md:flex ml-auto text-center justify-center py-20 px-5">
+        <form className="w-full max-w-lg" onSubmit={handleSubmit}>
+          <h2 className="text-2xl text-gray-600 font-bold text-center mb-10">
             Register
           </h2>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full  px-3 mb-6 md:mb-0'>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full  px-3 mb-6 md:mb-0">
               <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                for='grid-first-email'
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-first-email"
               >
                 Email
               </label>
               <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-                type='text'
-                placeholder='Jane@gmail.com'
-                id='email'
-                email='email'
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                type="text"
+                placeholder="Jane@gmail.com"
+                id="email"
+                email="email"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.email}
                 invalid={touched.email && errors.email ? true : false}
               />
               {touched.email && errors.email ? (
-                <p className='text-red-500 text-xs italic'>
+                <p className="text-red-500 text-xs italic">
                   Please fill out this field.
                 </p>
               ) : null}
             </div>
           </div>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full px-3'>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                for='grid-password'
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="password"
               >
                 Password
               </label>
-              <div class='flex flex-wrap -mx-3 mb-6'>
-                <div class='w-full px-3'>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
                   <input
-                    class='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                    id='grid-password'
-                    type='password'
-                    placeholder='******************'
-                    id='password'
-                    email='password'
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    type="password"
+                    placeholder="******************"
+                    id="password"
+                    email="password"
                     onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.password}
                     invalid={touched.password && errors.password ? true : false}
                   />
-                  <p class='text-gray-600 text-xs italic'>Capazy team</p>
+                  <p className="text-gray-600 text-xs italic">Capazy team</p>
                 </div>
               </div>
 
               {touched.password && errors.password ? (
-                <p className='text-red-500 text-xs italic'>
+                <p className="text-red-500 text-xs italic">
                   Please fill out this field.
                 </p>
               ) : null}
             </div>
           </div>
-          <div className='text-center pt-5'>
+          <div className="text-center pt-5">
             <button
-              type='submit'
-              className='btn bg-brand-blue-light hover:border-gray-500 shadow-lg hover:bg-indigo-100 '
+              type="submit"
+              className="btn bg-brand-blue-light hover:border-gray-500 shadow-lg hover:bg-indigo-100 "
             >
               Pend WhatsApp password
             </button>
---
+++ src/views/Signup/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const SignUp = () => {
+  const { handleSubmit, handleChange, values, errors, touched } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+      password: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className="w-full max-w-xs mx-auto">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="Email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={values.email}
+            invalid={touched.email && errors.email ? true : undefined}
+          />
+          <p className="text-red-500 text-xs italic">{errors.email}</p>
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={values.password}
+            invalid={touched.password && errors.password ? true : undefined}
+          />
+          <p className="text-red-500 text-xs italic">{errors.password}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button className="btn bg-brand-blue text-white mb-0" type="submit">
+            Sign Up
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="/#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
