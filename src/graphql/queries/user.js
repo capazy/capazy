675c89d176c3b633a954d7a5d8aeed325d873f68
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-05-09 16:13:26 -0500

    Joined and created projects

--- src/graphql/queries/user.js
+++ src/graphql/queries/user.js
@@ -1,20 +1,53 @@
-// import gql from 'graphql-tag';
+import gql from 'graphql-tag';
 
-// export const GET_USER = gql`
-//   query getUser(
-//     $id: ID!
-//   ) {
-//     createUser(
-//       userInput: {
-//         email: $email
-//         password: $password
-//         firstName: $firstName
-//         lastName: $lastName
-//       }
-//     ) {
-//       userId
-//         firstName
-//         lastName
-
-//     }
-//   }
+export const GET_USER_CREATED_PROJECTS = gql`
+  {
+    user {
+      createdProjects {
+        _id
+        title
+        description
+        type
+        deadline
+        published
+        isOpen
+        creator {
+          _id
+        }
+        vacancies {
+          _id
+          title
+          experience
+          skills
+        }
+      }
+    }
+  }
+`;
+export const GET_USER_JOINED_PROJECTS = gql`
+  {
+    user {
+      _id
+      joinedProjects {
+        title
+        experience
+        skills
+        project {
+          _id
+          title
+          description
+          type
+          deadline
+          published
+          isOpen
+          creator {
+            _id
+          }
+          vacancies {
+            _id
+          }
+        }
+      }
+    }
+  }
+`;
