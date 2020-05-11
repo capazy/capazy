f8b0822d402b0081385722b69a11e94aa3305854
Julian Urrego <julian.urrego.ar@gmail.com>
2020-05-11 17:15:23 -0400

    add: feed style

--- src/views/Feed/index.js
+++ src/views/Feed/index.js
@@ -1,102 +1,36 @@
 import React, { Fragment } from 'react';
 
 // apollo
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { GET_PROJECTS } from '../../graphql/queries/project';
-import { JOIN_VACANCY } from '../../graphql/mutation/vacancy';
+// import { JOIN_VACANCY } from '../../graphql/mutation/vacancy';
+import { CardProject } from '../../components';
 
 const Feed = () => {
-  const [joinVacancy] = useMutation(JOIN_VACANCY, {
-    update(_, { data }) {
-      console.log('Vacancy', data);
-    },
-  });
+  // const [joinVacancy] = useMutation(JOIN_VACANCY, {
+  //   update(_, { data }) {
+  //     console.log('Vacancy', data);
+  //   },
+  // });
 
   const { loading, data, refetch } = useQuery(GET_PROJECTS);
   if (loading) return <p>Loading...</p>;
   refetch();
 
-  const handleJoin = (vacancyId) => {
-    joinVacancy({ variables: { vacancyId } });
-  };
+  // const handleJoin = (vacancyId) => {
+  //   joinVacancy({ variables: { vacancyId } });
+  // };
 
   return (
     <Fragment>
-      <div className="py-16">
-        {data.projects.map((project) => (
-          <div
-            className="container w-full flex flex-wrap mx-auto px-2 lg:pt-2"
-            key={project._id}
-          >
-            <div className="w-full p-8 mt-6 lg:mt-0 text-gray-900 leading-normal bg-white border border-gray-400 border-rounded">
-              <div className="font-sans">
-                <h1 className="font-sans break-normal text-gray-900 py-2 text-xl">
-                  {project.title} | {project._id}
-                </h1>
-                <hr className="border-b border-gray-400" />
-              </div>
-              <p className="py-6">{project.description}</p>
-
-              <div>
-                <h2>Project Info</h2>
-                <table className="w-full">
-                  <thead>
-                    <tr className="text-left">
-                      <th>Type</th>
-                      <th>Deadline</th>
-                      <th>Published</th>
-                      <th>Vacancies</th>
-                      <th>Creator</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{project.type}</td>
-                      <td>{project.deadline}</td>
-                      <td>{project.published}</td>
-                      <td>{project.vacancies.length}</td>
-                      <td>{project.creator._id}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <br />
-
-              <div>
-                <h2>Vacancies Info</h2>
-                <table className="w-full">
-                  <thead>
-                    <tr className="text-left">
-                      <th>ID</th>
-                      <th>Title</th>
-                      <th>Experience Req.</th>
-                      <th>Skills</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {project.vacancies.map((vacancy) => (
-                      <tr key={vacancy._id}>
-                        <td>{vacancy._id}</td>
-                        <td>{vacancy.title}</td>
-                        <td>{vacancy.experience}</td>
-                        <td>{vacancy.skills}</td>
-                        <td>
-                          <button
-                            className="btn bg-brand-blue text-white mb-0"
-                            onClick={() => handleJoin(vacancy._id)}
-                          >
-                            Join
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+      <div class="container my-12 mx-auto px-4 md:px-12">
+        <div className="flex flex-wrap -mx-1 lg:-mx-4">
+          {data.projects.map((project) => (
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3  ">
+              <CardProject project={project} />
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </Fragment>
   );
