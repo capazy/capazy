fc3fa15f72b95b62ac90b31ec6b78678e3cd7965
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-05-20 13:57:33 -0500

    Fix conflict

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,5 @@ export { default as SelectOne } from './SelectOne';
 export { default as ProjectCard } from './ProjectCard';
 export { default as CreatedCard } from './CreatedCard';
 export { default as JoinedCard } from './JoinedCard';
-<<<<<<< HEAD
 export { default as UserCard } from './UserCard';
-=======
 export { default as Alert } from './Alert';
->>>>>>> 646295cf723f8bd6ff0613da4e439ab6b063cc32
