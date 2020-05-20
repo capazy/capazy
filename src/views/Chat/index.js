c5eee555eddb3d6fc4d93b17ca6f9e1fb7c6d08a
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-05-20 00:00:46 -0500

    Fetch user to sendbird

--- src/views/Chat/index.js
+++ src/views/Chat/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { App as SendBirdApp } from 'sendbird-uikit';
+
+// style
 import './index.css';
 
+// context
+import { UserContext } from '../../context/UserContext';
+
 const Chat = () => {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
       <SendBirdApp
         appId={'C372DF8C-8E5A-46C7-BC89-7F6060E5C941'}
-        userId="139386"
-        nickname="Sebas"
+        userId={user._id}
+        nickname={user.firstName}
       />
     </div>
   );
