b1d62e342ff3e58be4adada6a1046d962a7048cd
Julian Urrego <julian.urrego.ar@gmail.com>
2020-05-31 16:43:53 -0400

    Add: postuladed number on the table

--- src/components/CreatedCard/index.js
+++ src/components/CreatedCard/index.js
@@ -24,7 +24,7 @@ const CreatedCard = ({
               alt="project"
             />
             <div className="w-full">
-              <div className="flex justify-between w-full my-2">
+              <div className="flex justify-between w-full my-1">
                 <div>
                   <p className="text-gray-900 font-bold text-xl ">{title}</p>
                   <p className="text-sm text-gray-600 flex items-center mb-2">
@@ -32,28 +32,26 @@ const CreatedCard = ({
                   </p>
                 </div>
 
-                <div className=" text-center">
+                <div className="text-center">
                   <Link to={`/project/create?projectId=${_id}`}>
-                    <button className="btn-small bg-brand-blue mt-2">
-                      Edit
-                    </button>
+                    <button className="btn-small bg-brand-blue">Edit</button>
                   </Link>
                 </div>
               </div>
               <div>
                 <div className="flex justify-start">
                   <div className="text-center mt-1">
-                    <h2 className="text-xs font-semibold text-gray-800">
+                    <p className="text-xs font-semibold text-gray-800">
                       Start Date
-                    </h2>
+                    </p>
                     <p className="text-xs py-2 text-sm text-gray-700">
                       {startDate.slice(0, 10)}
                     </p>
                   </div>
                   <div className="text-center mt-1 ml-4">
-                    <h2 className="text-xs font-semibold text-gray-800">
+                    <p className="text-xs font-semibold text-gray-800">
                       End Date
-                    </h2>
+                    </p>
                     <p className="text-xs py-2 text-sm text-gray-700">
                       {endDate.slice(0, 10)}
                     </p>
@@ -65,25 +63,43 @@ const CreatedCard = ({
           </div>
 
           {/* TABLE */}
+
           <div className="bg-white rounded -mt-4">
-            <h1 className="text-md font-semibold text-gray-800">Vacancies</h1>
+            <div className="grid grid-cols-3 text-center border-b">
+              <div className="col-start-1">
+                {' '}
+                <h1 className="text-md font-semibold text-gray-600">
+                  Vacancies
+                </h1>
+              </div>
+              <div className="col-start-2">
+                {' '}
+                <h1 className="text-md font-semibold text-gray-600">
+                  Postuladed
+                </h1>
+              </div>
+              <div className="col-start-3">
+                {' '}
+                <h1 className="text-md font-semibold text-gray-600">Status</h1>
+              </div>
+            </div>
+
             <table className=" w-full border-collapse">
               <tbody>
                 {vacancies.map((vacancy) => (
                   <tr key={vacancy._id} className="w-full  ">
-                    <td className="tab border-b justify-arround">
+                    <td className="tab border-b ">
                       <input
                         className="absolute opacity-0"
                         id={vacancy._id}
                         type="checkbox"
                         name="tabs"
                       />
-                      <div className="flex">
-                        <span className="w-full py-1 ">
+                      <div className="grid grid-cols-3 gap-4">
+                        <div className="col-start-1 ">
                           <h1 className="block text-gray-800 text-sm">
                             {vacancy.title}
                           </h1>
-
                           {vacancy.skills.map((skill, i) => (
                             <span key={skill}>
                               <p
@@ -94,36 +110,36 @@ const CreatedCard = ({
                               </p>
                             </span>
                           ))}
-                        </span>
-                        <div className=" py-1">
-                          {!vacancy.selectedUser._id ? (
-                            <span className="inline-block bg-green-200 px-2 p-0 mt-4 text-sm rounded-full text-gray-700 mr-2">
-                              open
-                            </span>
-                          ) : (
-                            <span className="inline-block bg-red-200 px-2 p-0 mt-4 text-sm rounded-full text-gray-700 mr-2">
-                              closed
-                            </span>
-                          )}
                         </div>
-                        <div className=" py-1">
-                          <label
-                            className="block text-gray-800 text-sm leading-normal cursor-pointer"
-                            htmlFor={vacancy._id}
-                          >
-                            {/* <span className="inline-block bg-green-200 px-2 p-0 mt-4 text-sm rounded-full text-gray-700 mr-2">
-                              open
-                            </span> */}
-                            <div className="inset-y-0 right-0 flex text-center px-2 mt-4 text-gray-600">
-                              <svg
-                                className="fill-current h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                              >
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                              </svg>
-                            </div>
-                          </label>
+                        <div className="col-start-2 text-center text-sm mt-2 ">
+                          {vacancy.postulatedUsers.length || 0}
+                        </div>
+                        <div className="col-start-3 text-center ">
+                          <span className="flex justify-center">
+                            {!vacancy.selectedUser._id ? (
+                              <span className="inline-block bg-green-200 px-2 p-0 mt-4 text-sm rounded-full text-gray-700 mr-1">
+                                open
+                              </span>
+                            ) : (
+                              <span className="inline-block bg-red-200 px-2 p-0 mt-4 text-sm rounded-full text-gray-700 mr-1">
+                                closed
+                              </span>
+                            )}
+                            <label
+                              className="text-gray-800 text-sm leading-normal cursor-pointer"
+                              htmlFor={vacancy._id}
+                            >
+                              <div className=" text-center px-1 mt-4 text-gray-600">
+                                <svg
+                                  className="fill-current h-6 w-6"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                >
+                                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                </svg>
+                              </div>
+                            </label>
+                          </span>
                         </div>
                       </div>
 
